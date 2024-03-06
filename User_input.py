76adfa4181f5f2470114ac491041706881ea5ca6
SANDRA <smaerere@gmail.com>
2024-03-06 12:50:09 +0300

    <first committ>

---
+++ User_input.py
@@ -0,0 +1,9 @@
+# Get user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Print personalized message
+print("\nHello, " + name + "!")
+print("You are " + age + " years old, and you are located in " + location + ".")
+print("Have a great week!!!")
